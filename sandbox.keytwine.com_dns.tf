93f1739d24da91da07bc666abaf9a7ac02caaf65
Hector Rivas Gandara <keymon@gmail.com>
2017-11-11 17:27:09 +0000

    Wilcard domain for bosh-lite.sandbox.keytwine.com
    Add a wildcard domain for *.bosh-lite.sandbox.keytwine.com, which
    points to the default IP of the gorouter in the cf deployment,
    10.244.0.34.
    
    This is the same than the *.bosh-lite.com from pivotal, but it
    would allow me to generate my own certificates.

--- sandbox.keytwine.com_dns.tf
+++ sandbox.keytwine.com_dns.tf
@@ -2,6 +2,14 @@ resource "aws_route53_zone" "sandbox_keytwine_com" {
   name = "sandbox.keytwine.com"
 }
 
+resource "aws_route53_record" "bosh-lite_sandbox_keytwine_com" {
+  zone_id = "${aws_route53_zone.sandbox_keytwine_com.zone_id}"
+  name    = "*.bosh-lite.sandbox.keytwine.com"
+  type    = "A"
+  ttl     = "300"
+  records = ["10.244.0.34"]
+}
+
 output "sandbox_keytwine_com_ns" {
   value = "${aws_route53_zone.sandbox_keytwine_com.name_servers}"
 }
